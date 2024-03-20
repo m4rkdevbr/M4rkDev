ea64407d3dd8bcf3d2c69298c93f1df3bbe6c411
Fábio Marques <157427481+m4rkdevbr@users.noreply.github.com>
2024-03-20 14:15:32 -0300

    Update exercicio3.js

--- Arrays e Loops/exercicio3.js
+++ Arrays e Loops/exercicio3.js
@@ -7,34 +7,3 @@ for (let i = 0; i < numeros.length; i++) {
 }
 
 console.log(soma);
-
-
-
-
-
-
-
-
-
-
-
-
-//cria uma variável chamada soma e atribui o valor 0 a ela // incializa avariavel que guarda um indice
-//
-//for (let x = 0; x < numeros.length; x++) {
-//    soma = soma + numeros[x];
-//    console.log(soma);
-//}
-
-//Agora vamos somar todos os numeros do array.
-//const number = ['55', '2', '87', '143', '5', '34'];
-
-//inicializa a variável i com 0; enquanto i for menor que 6, executa o bloco de código; incrementa i em 1 //antes de tudo - a pergunta(execução) - vai pra execução - incremento
-
-//let soma = 0;                                   //cria uma variável chamada soma e atribui o valor 0 a ela // incializa avariavel que guarda um indice
-//
-//for (let x = 0; x < number.length; x++) {
-//    soma = soma + number[x];
-//    console.log(soma);
-//    x++;
-//}
